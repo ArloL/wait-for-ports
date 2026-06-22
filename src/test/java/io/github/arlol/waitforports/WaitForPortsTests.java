89743496cc6e550cf523bfd5a6a2e4474281e4e8
Arlo O'Keeffe <tiger@k5d.de>
2026-06-22 12:30:05 +0200

    use integration test

---
+++ src/test/java/io/github/arlol/waitforports/WaitForPortsTests.java
@@ -0,0 +1,14 @@
+package io.github.arlol.waitforports;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class WaitForPortsTests {
+
+	@Test
+	public void isTrue() throws Exception {
+		assertTrue(true);
+	}
+
+}
